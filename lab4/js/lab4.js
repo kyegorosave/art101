a8179aa088c4b442c62786fcb9404998e372d914
Kye Gorosave <kye.goro@icloud.com>
2023-10-23 23:29:02 -0700

    laab 4 changes

---
+++ lab4/js/lab4.js
@@ -0,0 +1,17 @@
+//Rock-Paper-Scissors Game
+//Kye Gorosave <kgorosav@ucsc.edu>
+//Mon Oct. 23 2023
+
+//Explain the rules of the game
+  //Paper loses to scissors, rock loses to paper, and scissors lose to rock. 
+//Ask the player for their choice at the beginning of each turn
+  //Choose rock or paper or scissors
+//Generate a random choice for either rock, paper, or scissors.
+  //random function with a choice of either rock, paper, or scissors
+//Compare player vs computer choice
+  //if(choice=same) then display “ITS A DRAW”
+      //else if player chooses rock and computer scissors, OR player chooses scissors and computer chooses paper, OR player chooses paper and computer chooses rock DISPLAY “YOU WIN!”
+  //else display “COMPUTER WINS!”
+//Show computers choice and see if it was a win, draw, or loss and then ask them if they want to play again
+
+
